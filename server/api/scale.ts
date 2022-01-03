949e6d9a06a3c410cf9262d4373800b643815817
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-03 22:30:05 +0530

    scale is set to todo

---
+++ server/api/scale.ts
@@ -0,0 +1,5 @@
+import jimp from 'jimp'
+
+export default async function scale(file: string) {
+  // do here using reference: https://github.com/oliver-moran/jimp/tree/master/packages/plugin-scale
+}
