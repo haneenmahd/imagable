b0635969f7fe81a9e2fe3d9f05f7f152401933a2
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-03 22:02:51 +0530

    add color function

--- server/types/types.ts
+++ server/types/types.ts
@@ -2,3 +2,10 @@ export interface BlitCoordinates {
     x: number;
     y: number;
 }
+
+interface Color {
+    apply: string;
+    params: number[] | string[];
+}
+
+export type ColorOptions = Color[];
