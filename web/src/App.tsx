525084f184c835981e743bf344f2fb877a810d18
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-04 19:40:09 +0530

    add basic ui styles

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/svg/logo.svg';
 import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <img src={logo} className="App-logo" alt="logo" />
+      <h1>Imagable</h1>
     </div>
   );
 }
