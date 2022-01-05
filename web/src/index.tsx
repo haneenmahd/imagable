b8f4c6b27ffd5ace449b647cbdce2b006967feeb
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-05 16:52:09 +0530

    UI: setup routes

--- web/src/index.tsx
+++ web/src/index.tsx
@@ -1,14 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter, Routes, Route, Outlet } from 'react-router-dom';
 import './index.scss';
 
 import App from './App';
 import NavBar from './components/NavBar';
 
+// pages
+import Tools from './pages/Tools';
+import Create from './pages/Create';
+
+/**
+ * This is the app component which is going to be the renderer for all child elements inside the app.
+ */
+function RenderingApp() {
+  return (
+    <div>
+      <NavBar />
+      <Outlet />
+    </div>
+  );
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <NavBar />
-    <App />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<RenderingApp />}>
+          <Route index element={<App />} />
+          <Route path="/tools" element={<Tools />} />
+          <Route path="/create" element={<Create />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
