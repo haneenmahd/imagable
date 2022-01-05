988b6d347d029454894630c72e3e76c0701526a3
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-05 16:44:28 +0530

    UI: Create NavBar design

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import styled, { css } from "styled-components";
 
+// NavContentIcons
+import NavContentToolsIcon from "../assets/svg/tools.svg";
+import NavContentCreateIcon from "../assets/svg/create.svg";
+import NavContentGitHubIcon from "../assets/svg/github.svg";
+
 const NavMobileStyle = css`
     width: 75%
 `;
@@ -23,13 +28,58 @@ const NavBarComponent = styled.nav`
     }
 `;
 
-const NavContent = styled.div``;
+const NavContent = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+    align-items: center;
+    padding: 10px;
+    cursor: pointer;
+    margin: 0 4px;
+    border-radius: 20px;
+    transition: background 200ms ease-out;
+    &:hover {
+        background: rgba(255, 255, 255, 0.88);;
+    }
+
+    img {
+        margin: 0 4px;
+    }
+`;
+
+const NavContentText = styled.p`
+    font-weight: bold;
+    font-size: 20px;
+    line-height: 31px;
+    color: #768DA1;
+    margin: 0 4px;
+`;
 
 export default class NavBar extends React.Component {
     render() {
         return(
             <NavBarComponent>
 
+                <NavContent>
+                    <img src={NavContentToolsIcon} alt="NavBar Tools Object NavLink" />
+                    <NavContentText>
+                        Tools
+                    </NavContentText>
+                </NavContent>
+
+                <NavContent>
+                    <img src={NavContentCreateIcon} alt="NavBar Tools Object NavLink" />
+                    <NavContentText>
+                        Create
+                    </NavContentText>
+                </NavContent>
+
+                <NavContent>
+                    <img src={NavContentGitHubIcon} alt="NavBar Tools Object NavLink" />
+                    <NavContentText>
+                        GitHub
+                    </NavContentText>
+                </NavContent>
             </NavBarComponent>
         );
     }
