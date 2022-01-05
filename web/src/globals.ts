0834ecdc81add44eb64ba0185f50d5e4de70c63d
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-05 18:00:52 +0530

    add global variables

---
+++ web/src/globals.ts
@@ -0,0 +1,7 @@
+// global variables that should be used under specific views or components
+// that re-use the same thing again and again
+const globals = {
+  githubUrl: "https://github.com/imagable",
+};
+
+export default globals;
