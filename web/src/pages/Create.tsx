d2f9879a501ccc125fcbec9abb998176ed246579
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-05 16:52:17 +0530

    setup basic pages

---
+++ web/src/pages/Create.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Create extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Create Page</h1>
+                <p>Incomplete</p>
+            </div>
+        );
+    }
+}
---
+++ web/src/pages/Tools.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default class Tools extends React.Component {
+    render() {
+        return(
+            <div>
+                <h1>Tools Page</h1>
+                <p>Incomplete</p>
+            </div>
+        );
+    }
+}
