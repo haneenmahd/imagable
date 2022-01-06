e28afb312f48da006e10899135d3ff8c0334af9c
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-06 17:22:32 +0530

    set `Page` style to use `80px` of padding

--- web/src/styles/Page.ts
+++ web/src/styles/Page.ts
@@ -1,11 +1,7 @@
 import styled from "styled-components";
 
 const Page = styled.div`
-  padding: 50px 80px;
-
-  @media screen and (max-width: 600px) {
-    padding: 20px 30px;
-  }
+  padding: 80px;
 `;
 
 export default Page;
