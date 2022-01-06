43d70384d9caf9d86c406af31044f5fae1c59662
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-06 16:57:29 +0530

    mount Layout to App

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,17 +1,12 @@
-import React from "react";
 // NOTE: fix this after basic ui implementation import logo from "./assets/png/logo.png";
 import "./App.scss";
+
+import Layout from "./layout/Layout";
 
 function App() {
   return (
     <div className="App">
-      <div className="main-header">
-        {/**NOTE: fix this after basic ui implementation <img src={logo} alt="Imagable Logo" /> */}
-        <h1>Imagable</h1>
-        <h3>
-          Create, Edit and do <br />more with your images
-        </h3>
-      </div>
+      <Layout />
     </div>
   );
 }
