92277a5454750acfa866991770357a81526795d4
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-06 17:17:46 +0530

    Add Page Style for throughout the website

---
+++ web/src/styles/Page.ts
@@ -0,0 +1,11 @@
+import { css } from "styled-components";
+
+const Page = css`
+  padding: 50px 80px;
+
+  @media screen and (max-width: 600px) {
+    padding: 20px 30px;
+  }
+`;
+
+export default Page;
