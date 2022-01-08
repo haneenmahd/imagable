0cdda736b0cb15838243af6bcc98364a5a5518c6
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-08 09:21:09 +0530

    add hover effects

--- web/src/styles/Button.tsx
+++ web/src/styles/Button.tsx
@@ -1,12 +1,20 @@
-import React from "react";
 import styled, { css } from "styled-components";
 
 const DefaultStyles = css`
     background: #FFEFEF;
+
+    &:hover {
+      background: #e6d2d2;
+    }
 `;
 
 const MainStyles = css`
     background: #92A9BD;
+    color: #FFEFEF;
+
+    &:hover {
+      background: #5c7183;
+    }
 `;
 
 interface ButtonStylesProps {
@@ -21,11 +29,14 @@ const ButtonStyle = styled.button<ButtonStylesProps>`
     padding: 15px;
     ${p => p.theme === "main" ? MainStyles : DefaultStyles}
     border-radius: 20px;
+    margin: 5px;
+    cursor: pointer;
+    transition: background ease-in .1s;
 `;
 
 const ButtonIcon = styled.span`
     height: 20px; width: 20px;
-    margin 0 5px;
+    margin: 0 5px;
 
     & svg, img {
         height: 20px; width: 20px;
@@ -50,47 +61,7 @@ const Button = (props: ButtonProps) => {
             {/* Render only if `props.icon` exists */}
             <ButtonText>{props.text}</ButtonText>
         </ButtonStyle>
-=======
-import styled from "styled-components";
-
-const ButtonStyle = styled.button`
-  display: flex;
-  flex-direction: row;
-  justify-content: center;
-  align-items: center;
-  padding: 15px;
-  background: #ffefef;
-  border-radius: 20px;
-`;
-
-const ButtonIcon = styled.span`
-  margin: 0px 5px;
-  height: 20px;
-  width: 20px;
-`;
-
-const ButtonText = styled.p`
-  font-family: DM Sans;
-  font-size: 21px;
-  font-weight: 500;
-  color: #768da1;
-  margin: 0 5px;
-`;
-
-interface ButtonProps {
-  text: string;
-  icon?: JSX.Element;
-}
-
-const Button = (props: ButtonProps): JSX.Element => {
-    return (
-      <ButtonStyle>
-        {/* Render only if `props.icon` exists */}
-        {props.icon && <ButtonIcon>{props.icon}</ButtonIcon>}
-        {/* Render only if `props.icon` exists */}
-        <ButtonText>{props.text}</ButtonText>
-      </ButtonStyle>
-    );
+    )
 }
 
 export default Button;
