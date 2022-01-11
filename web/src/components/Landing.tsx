cfdfeceebbfe15030931c44d66a381a98230bd64
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-11 20:49:29 +0530

    Rename `Layout` to `Landing`

--- web/src/components/Landing.tsx
+++ web/src/components/Landing.tsx
@@ -1,14 +1,14 @@
 import styled from "styled-components";
-import Actions from "../components/ActionsContainer";
+import Actions from "./ActionsContainer";
 
 // Styled Component Tokens
 import Tokens from "../styles/tokens";
 
 // Logo
 import Logo from "../assets/svg/logo.svg";
-import ImageDiamond from "../components/ImageDiamond";
+import ImageDiamond from "./ImageDiamond";
 
-const LayoutContainer = styled.main`
+const LandingContainer = styled.main`
   max-width: 100vw;
   min-height: 100vh;
   display: flex;
@@ -21,7 +21,7 @@ const LayoutContainer = styled.main`
   }
 `;
 
-const LayoutContent = styled.div`
+const LandingContent = styled.div`
   width: 46vw;
   min-height: 100vh;
   display: flex;
@@ -34,41 +34,41 @@ const LayoutContent = styled.div`
   }
 `;
 
-const LayoutImage = styled.img`
+const LandingImage = styled.img`
   height: 200px;
   margin: 0;
 `;
 
-const LayoutHeader = styled.h1`
+const LandingHeader = styled.h1`
   ${Tokens.MainHeaderStyle}
 
   ${Tokens.MainHeaderStyleMobile}
 `;
 
-const LayoutShortHeader = styled.h3`
+const LandingShortHeader = styled.h3`
   ${Tokens.SecondaryHeaderStyle}
 
   ${Tokens.SecondaryHeaderStyleMobile}
     ${Tokens.SecondaryHeaderStyleMobileSmall}
 `;
 
-const Layout = () => {
+const Landing = () => {
   return (
-    <LayoutContainer>
-      <LayoutContent>
-        <LayoutImage src={Logo} />
-        <LayoutHeader>Imagable</LayoutHeader>
-        <LayoutShortHeader>
+    <LandingContainer>
+      <LandingContent>
+        <LandingImage src={Logo} />
+        <LandingHeader>Imagable</LandingHeader>
+        <LandingShortHeader>
           Create, Edit and do <br />
           more with your images
-        </LayoutShortHeader>
+        </LandingShortHeader>
         <Actions />
-      </LayoutContent>
-      <LayoutContent>
+      </LandingContent>
+      <LandingContent>
         <ImageDiamond />
-      </LayoutContent>
-    </LayoutContainer>
+      </LandingContent>
+    </LandingContainer>
   );
 };
 
-export default Layout;
+export default Landing;
