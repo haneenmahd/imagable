34b5163d7c5ebce939e75e2a9e03a73605352462
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-12 08:44:03 +0530

    Add Footer style

---
+++ web/src/components/Footer.tsx
@@ -0,0 +1,22 @@
+import styled from "styled-components";
+
+const FooterStyle = styled.footer`
+  display: flex;
+  flex-direction: row;
+  align-items: flex-start;
+  padding: 76px 63px;
+  background: linear-gradient(180deg, rgba(32, 32, 32, 0.94) 0%, #202020 100%);
+  border-radius: 40px;
+  width: 90vw;
+  margin: 40px calc(100vw / 20);
+`;
+
+const Footer = () => {
+    return (
+        <FooterStyle>
+
+        </FooterStyle>
+    );
+}
+
+export default Footer;
