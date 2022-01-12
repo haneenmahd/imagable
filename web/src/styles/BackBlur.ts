d25d95b5743ff70b7ee3fbab4478e3da941bc159
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-12 15:40:43 +0530

    Increase `BackBlur` blur

--- web/src/styles/BackBlur.ts
+++ web/src/styles/BackBlur.ts
@@ -2,7 +2,7 @@ import { css } from "styled-components";
 
 const BackBlur = css`
     -webkit-backdrop-filter: blur(20px);
-    backdrop-filter: blur(20px);
+    backdrop-filter: blur(30px);
 `;
 
 export default BackBlur;
