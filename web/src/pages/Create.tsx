93f02673cb6f8c11f0795dda6e9ec4b73dee1e4e
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-12 16:25:37 +0530

    Add Create page

--- web/src/pages/Create.tsx
+++ web/src/pages/Create.tsx
@@ -1,15 +1,68 @@
 import React from "react";
 import styled from "styled-components";
+import BackBlur from "../styles/BackBlur";
 import Page from "../styles/Page";
 
-const PageContainer = styled(Page)``;
+import Sunset from "../assets/png/sunset.png";
 
-export default class Create extends React.Component {
+const PageContainer = styled(Page)`
+    min-height: 100vh;
+    max-width: 100vw;
+    padding: 0;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+`;
+
+const ToolsSection = styled.div<{ show: boolean; }>`
+    display: ${p => p.show ? "default" : "none"};
+    position: fixed;
+    top: 50%;
+    left: 7%;
+    transform: translate(-7%, -50%);
+    height: 80vh;
+    width: 50vw;
+    background: rgba(0, 0, 0, 0.5);
+    border-radius: 30px;
+    z-index: 200;
+    ${BackBlur}
+`;
+
+const ImageCanvas = styled.img`
+    max-height: 85vh;
+    width: auto;
+    border-radius: 30px;
+    filter: blur(1px) contrast(500px);
+    box-shadow: 20px 40px 50px 0 #c4c4c4aa;
+`;
+
+export default class Create extends React.Component<{}, { isToolsOpen: boolean; }> {
+    constructor(props: any) {
+        super(props);
+
+        this.state = {
+            isToolsOpen: true
+        };
+
+        this.setToolsOpen = this.setToolsOpen.bind(this);
+    }
+
+    setToolsOpen() {
+        this.setState({
+            isToolsOpen: !this.state.isToolsOpen
+        });
+    }
+
     render() {
+        const toolsOpen = this.state.isToolsOpen;
+
         return(
             <PageContainer>
-                <h1>Create Page</h1>
-                <p>Incomplete</p>
+                <ToolsSection show={toolsOpen}>
+
+                </ToolsSection>
+                
+                <ImageCanvas src={Sunset} />
             </PageContainer>
         );
     }
