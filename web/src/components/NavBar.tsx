f9a45efeeb70cd5bbf18892fdb3e485b942ee4b7
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-12 07:53:33 +0530

    Use Blur Effect while scrollposition > 300

--- web/src/components/NavBar.tsx
+++ web/src/components/NavBar.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled, { css } from "styled-components";
 import { Link } from "react-router-dom";
 
@@ -11,86 +11,120 @@ import NavContentCreateIcon from "../assets/svg/create.svg";
 import NavContentGitHubIcon from "../assets/svg/github.svg";
 import globals from "../globals";
 
-const NavMobileStyle = css`
-    width: 75%;
+const NavScrolledEffect = css`
+  background-color: rgba(255, 255, 255, 0.4);
+  backdrop-filter: blur(20px);
+  -webkit-backdrop-filter: blur(20px);
 `;
 
-const NavBarComponent = styled.nav`
-    position: fixed;
-    top: 3%;
-    left: 50%;
-    transform: translate(-50%, -3%);
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    padding: 0px;
-    width: 45%;
-    height: 43px;
+const NavMobileStyle = css`
+  width: 100%;
+  top: 0%;
+  height: 60px;
+`;
 
-    @media screen and (max-width: 600px) {
-        ${NavMobileStyle}
-    }
+interface NavBarComponentProps {
+  scrolled: boolean;
+}
+
+const NavBarComponent = styled.nav<NavBarComponentProps>`
+  position: fixed;
+  top: 3%;
+  left: 50%;
+  transform: translate(-50%, -3%);
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 0px;
+  width: 45%;
+  height: 43px;
+  ${p => p.scrolled && NavScrolledEffect}
+
+  @media screen and (max-width: 600px) {
+    ${NavMobileStyle}
+  }
 `;
 
 const NavContent = styled.div`
-    display: flex;
-    flex-direction: row;
-    justify-content: center;
-    align-items: center;
-    padding: 10px;
-    cursor: pointer;
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  align-items: center;
+  padding: 10px;
+  cursor: pointer;
+  margin: 0 4px;
+  border-radius: 20px;
+  transition: background 200ms ease-out;
+  &:hover {
+    background: rgba(255, 255, 255, 0.88);
+  }
+
+  img {
     margin: 0 4px;
-    border-radius: 20px;
-    transition: background 200ms ease-out;
-    &:hover {
-        background: rgba(255, 255, 255, 0.88);;
-    }
+  }
+
+  @media screen and (max-width: 350px) {
+    padding: 6px;
 
     img {
-        margin: 0 4px;
+      height: 18px;
+      width: 18px;
+      margin: 0 2px;
     }
+  }
 `;
 
 const NavContentText = styled.p`
-    font-weight: bold;
-    font-size: 20px;
-    line-height: 31px;
-    color: #768DA1;
-    margin: 0 4px;
+  font-weight: bold;
+  font-size: 20px;
+  line-height: 31px;
+  color: #768da1;
+  margin: 0 4px;
+
+  @media screen and (max-width: 600px) {
+    font-size: 100%;
+  }
 `;
 
-export default class NavBar extends React.Component {
-    render() {
-        return(
-            <NavBarComponent>
-                <Link to="/tools">
-                    <NavContent>
-                        <img src={NavContentToolsIcon} alt="NavBar Tools Object NavLink" />
-                        <NavContentText>
-                            Tools
-                        </NavContentText>
-                    </NavContent>
-                </Link>
+const NavBar = () => {
+  const [scrollState, setScrollState] = useState<boolean>(false);
 
-                <Link to="/create">
-                    <NavContent>
-                        <img src={NavContentCreateIcon} alt="NavBar Tools Object NavLink" />
-                        <NavContentText>
-                            Create
-                        </NavContentText>
-                    </NavContent>
-                </Link>
+  // use blur effect while scroll position is above 300
+  window.onscroll = () => {
+      if (window.scrollY > 300) {
+          setScrollState(true);
+      } else {
+        setScrollState(false);
+      }
 
-                <LocalLink target="_blank" href={globals.githubUrl} noStyles={false}>
-                    <NavContent>
-                    <img src={NavContentGitHubIcon} alt="NavBar Tools Object NavLink" />
-                    <NavContentText>
-                        GitHub
-                    </NavContentText>
-                </NavContent>
-                </LocalLink>
-            </NavBarComponent>
-        );
-    }
-}
+      console.log(scrollState);
+  }
+
+  return (
+    <NavBarComponent scrolled={scrollState}>
+      <Link to="/tools">
+        <NavContent>
+          <img src={NavContentToolsIcon} alt="NavBar Tools Object NavLink" />
+          <NavContentText>Tools</NavContentText>
+        </NavContent>
+      </Link>
+
+      <Link to="/create">
+        <NavContent>
+          <img src={NavContentCreateIcon} alt="NavBar Tools Object NavLink" />
+          <NavContentText>Create</NavContentText>
+        </NavContent>
+      </Link>
+
+      <LocalLink target="_blank" href={globals.githubUrl} noStyles={false}>
+        <NavContent>
+          <img src={NavContentGitHubIcon} alt="NavBar Tools Object NavLink" />
+          <NavContentText>GitHub</NavContentText>
+        </NavContent>
+      </LocalLink>
+    </NavBarComponent>
+  );
+};
+
+export default NavBar;
