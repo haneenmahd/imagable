0f0535519ec6628efa96bc454d236e9331bdf6d0
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-12 17:02:05 +0530

    Test  inside editor

--- web/src/pages/Create.tsx
+++ web/src/pages/Create.tsx
@@ -4,6 +4,7 @@ import BackBlur from "../styles/BackBlur";
 import Page from "../styles/Page";
 
 import Sunset from "../assets/png/sunset.png";
+import Slider from "../styles/Slider";
 
 const PageContainer = styled(Page)`
     min-height: 100vh;
@@ -25,6 +26,7 @@ const ToolsSection = styled.div<{ show: boolean; }>`
     background: rgba(0, 0, 0, 0.5);
     border-radius: 30px;
     z-index: 200;
+    padding: 50px;
     ${BackBlur}
 `;
 
@@ -36,34 +38,42 @@ const ImageCanvas = styled.img`
     box-shadow: 20px 40px 50px 0 #c4c4c4aa;
 `;
 
-export default class Create extends React.Component<{}, { isToolsOpen: boolean; }> {
-    constructor(props: any) {
-        super(props);
+export default class Create extends React.Component<{}, { isToolsOpen: boolean, sliderValue: any; }> {
+  constructor(props: any) {
+    super(props);
 
-        this.state = {
-            isToolsOpen: true
-        };
+    this.state = {
+      isToolsOpen: true,
+      sliderValue: 0,
+    };
 
-        this.setToolsOpen = this.setToolsOpen.bind(this);
-    }
+    this.setToolsOpen = this.setToolsOpen.bind(this);
+    this.setSliderValue = this.setSliderValue.bind(this);
+  }
 
-    setToolsOpen() {
-        this.setState({
-            isToolsOpen: !this.state.isToolsOpen
-        });
-    }
+  setToolsOpen() {
+    this.setState({
+      isToolsOpen: !this.state.isToolsOpen,
+    });
+  }
 
-    render() {
-        const toolsOpen = this.state.isToolsOpen;
+  setSliderValue(value: number | readonly number[]) {
+    this.setState({
+      sliderValue: value,
+    });
+  }
 
-        return(
-            <PageContainer>
-                <ToolsSection show={toolsOpen}>
+  render() {
+    const state = this.state;
 
-                </ToolsSection>
-                
-                <ImageCanvas src={Sunset} />
-            </PageContainer>
-        );
-    }
+    return (
+      <PageContainer>
+        <ToolsSection show={state.isToolsOpen}>
+          <Slider currentIndex={state.sliderValue} onChange={(v) => this.setSliderValue(v)} />
+        </ToolsSection>
+
+        <ImageCanvas src={Sunset} />
+      </PageContainer>
+    );
+  }
 }
