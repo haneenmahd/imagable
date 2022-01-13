eba0a64e131bfb57bc4d7a3a9c395ff76f863863
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-13 20:06:00 +0530

    Finish `TabBar` design

--- web/src/components/TabBar.tsx
+++ web/src/components/TabBar.tsx
@@ -1,7 +1,11 @@
+import { useState } from "react";
 import styled from "styled-components";
 import CropIcon from "../assets/js/CropIcon";
+import DropIcon from "../assets/js/DropIcon";
+import FitlersIcon from "../assets/js/FiltersIcon";
 import SunIcon from "../assets/js/SunIcon";
 import VectorIcon from "../assets/js/VectorIcon";
+import Tab from "./Tab";
 
 const TabBarStyle = styled.div`
   display: flex;
@@ -22,11 +26,29 @@ const TabBarStyle = styled.div`
 `;
 
 const TabBar = () => {
+  const [activeTab, setActiveTab] = useState(0);
+
   return (
     <TabBarStyle>
-      <CropIcon />
-      <SunIcon />
-      <VectorIcon />
+      <Tab keyIndex={0} activeIndex={activeTab} setActiveIndex={setActiveTab}>
+        <SunIcon />
+      </Tab>
+
+      <Tab keyIndex={1} activeIndex={activeTab} setActiveIndex={setActiveTab}>
+        <CropIcon />
+      </Tab>
+
+      <Tab keyIndex={2} activeIndex={activeTab} setActiveIndex={setActiveTab}>
+        <DropIcon />
+      </Tab>
+
+      <Tab keyIndex={3} activeIndex={activeTab} setActiveIndex={setActiveTab}>
+        <VectorIcon />
+      </Tab>
+
+      <Tab keyIndex={4} activeIndex={activeTab} setActiveIndex={setActiveTab}>
+        <FitlersIcon />
+      </Tab>
     </TabBarStyle>
   );
 };
