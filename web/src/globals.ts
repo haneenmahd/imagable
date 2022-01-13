38458a7001fa3f4cfcb43c475e223d29d5ac8d81
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-13 14:04:19 +0530

    Add global `main Transition`

--- web/src/globals.ts
+++ web/src/globals.ts
@@ -2,6 +2,9 @@
 // that re-use the same thing again and again
 const globals = {
   githubUrl: "https://github.com/imagable",
+  styling: {
+    transition: "all 0.3s cubic-bezier(0.44, 0.18, 0.22, 0.82)"
+  }
 };
 
 export default globals;
