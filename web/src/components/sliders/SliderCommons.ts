535dc51a6e86adb0de7fc05561d68d3c51ed1f97
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-14 16:46:36 +0530

    Add SliderProps interface

--- web/src/components/sliders/SliderCommons.ts
+++ web/src/components/sliders/SliderCommons.ts
@@ -49,6 +49,12 @@ export interface SliderCommonsProps {
   };
 }
 
+export interface SliderProps {
+  keyIndex: number;
+  activeIndex: number;
+  setActiveIndex: React.Dispatch<React.SetStateAction<number>>;
+}
+
 export const stepperCounts: number[] = [
   0, 10, 15, 20, 25, 30, 35, 40, 45, 50, 55, 60, 65, 70, 75, 80, 85, 90, 95,
   100,
