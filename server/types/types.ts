0c25b0f11fe4718c7bc07239eb58327607ae3f0e
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-15 20:51:42 +0530

    add server json data type `IconSizeData`

--- server/types/types.ts
+++ server/types/types.ts
@@ -2,3 +2,23 @@ export interface ResizeOptions {
 	height: number;
 	width: number;
 }
+
+export interface IconSizes {
+	name: string;
+	resolutions: {
+		[ratio: string]: {
+			width: number;
+			height: number;
+		}
+	}
+}
+
+export interface IconSizePlatformData {
+	iconPlatform: string;
+	iconSizes?: IconSizes[];
+	spotlightIconSizes?: IconSizes[];
+	settingsIconSizes?: IconSizes[];
+	notificationIconSizes?: IconSizes[];
+}
+
+export type IconSizeData = IconSizePlatformData[];
