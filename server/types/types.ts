66c538bbeb5b9c4339e071643bd36d9053bfc953
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-15 15:32:25 +0530

    remove invalid types (#33)

--- server/types/types.ts
+++ server/types/types.ts
@@ -1,28 +1,4 @@
-export interface BlitCoordinates {
-	x: number;
-	y: number;
-}
-
-interface Color {
-	apply: string;
-	params: number[] | string[];
-}
-
 export interface ResizeOptions {
 	height: number;
 	width: number;
 }
-
-export interface OptimizeOptions {
-	height: number;
-	width: number;
-	quality: number;
-	outputPath: string;
-}
-
-export interface ScaleOptions {
-	height: number;
-	width: number;
-}
-
-export type ColorOptions = Color[];
