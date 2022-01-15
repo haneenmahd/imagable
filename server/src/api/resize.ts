14fca56318690e63cf4dc113099be8faa5f0f896
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-15 20:05:19 +0530

    Prettier 🎊

--- server/src/api/resize.ts
+++ server/src/api/resize.ts
@@ -9,12 +9,12 @@ export default async function resize(
 	file: string,
 	resizeOptions: ResizeOptions = {
 		height: Jimp.AUTO,
-		width: 400
+		width: 400,
 	}
 ) {
 	const image = await Jimp.read(file);
 
 	image.resize(resizeOptions.width, resizeOptions.height);
-	
+
 	await image.writeAsync(file);
 }
