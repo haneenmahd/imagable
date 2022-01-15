2835d463ed59113936bb3f2e0635907bf4e93ec1
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-15 08:31:31 +0530

    Prettier 🎊

--- web/src/styles/AllCenter.ts
+++ web/src/styles/AllCenter.ts
@@ -1,9 +1,9 @@
-import { css } from "styled-components";
+import {css} from "styled-components";
 
 const AllCenter = css`
-    display: flex;
-    align-items: center;
-    justify-content: center;
+  display: flex;
+  align-items: center;
+  justify-content: center;
 `;
 
 export default AllCenter;
