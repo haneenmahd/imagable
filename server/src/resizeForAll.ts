a466a47e7892d2748077a68c39a72219aae6c5f6
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-16 19:07:28 +0530

    add spotlight, settings and notifications icon

--- server/src/resizeForAll.ts
+++ server/src/resizeForAll.ts
@@ -22,8 +22,6 @@ export default async (file: string) => {
 				);
 
 				await resize(file, icon.resolutions['1x'], outputFileName);
-
-				console.log(platform.iconPlatform, icon.name, 'Done');
 			} else if (icon.resolutions['2x'] !== undefined) {
 				const outputFileName = path.resolve(
 					`${path.dirname(file)}/icon-set-imagable`,
@@ -33,8 +31,6 @@ export default async (file: string) => {
 				);
 
 				await resize(file, icon.resolutions['2x'], outputFileName);
-
-				console.log(platform.iconPlatform, icon.name, 'Done');
 			} else if (icon.resolutions['3x'] !== undefined) {
 				const outputFileName = path.resolve(
 					`${path.dirname(file)}/icon-set-imagable`,
@@ -44,13 +40,100 @@ export default async (file: string) => {
 				);
 
 				await resize(file, icon.resolutions['3x'], outputFileName);
-
-				console.log(platform.iconPlatform, icon.name, 'Done');
 			}
 		});
 
-		platform.notificationIconSizes?.forEach((size) => {
-			console.log(size.resolutions['2x'].width);
+		platform.notificationIconSizes?.forEach(async (icon) => {
+			if (icon.resolutions['1x'] !== undefined) {
+				const outputFileName = path.resolve(
+					`${path.dirname(file)}/icon-set-imagable`,
+					platform.iconPlatform,
+					icon.name,
+					`${icon.resolutions['1x']?.height}x${icon.resolutions['1x']?.width}.png`
+				);
+
+				await resize(file, icon.resolutions['1x'], outputFileName);
+			} else if (icon.resolutions['2x'] !== undefined) {
+				const outputFileName = path.resolve(
+					`${path.dirname(file)}/icon-set-imagable`,
+					platform.iconPlatform,
+					icon.name,
+					`${icon.resolutions['2x']?.height}x${icon.resolutions['2x']?.width}.png`
+				);
+
+				await resize(file, icon.resolutions['2x'], outputFileName);
+			} else if (icon.resolutions['3x'] !== undefined) {
+				const outputFileName = path.resolve(
+					`${path.dirname(file)}/icon-set-imagable`,
+					platform.iconPlatform,
+					icon.name,
+					`${icon.resolutions['3x']?.height}x${icon.resolutions['3x']?.width}.png`
+				);
+
+				await resize(file, icon.resolutions['3x'], outputFileName);
+			}
+		});
+
+		platform.settingsIconSizes?.forEach(async (icon) => {
+			if (icon.resolutions['1x'] !== undefined) {
+				const outputFileName = path.resolve(
+					`${path.dirname(file)}/icon-set-imagable`,
+					platform.iconPlatform,
+					icon.name,
+					`${icon.resolutions['1x']?.height}x${icon.resolutions['1x']?.width}.png`
+				);
+
+				await resize(file, icon.resolutions['1x'], outputFileName);
+			} else if (icon.resolutions['2x'] !== undefined) {
+				const outputFileName = path.resolve(
+					`${path.dirname(file)}/icon-set-imagable`,
+					platform.iconPlatform,
+					icon.name,
+					`${icon.resolutions['2x']?.height}x${icon.resolutions['2x']?.width}.png`
+				);
+
+				await resize(file, icon.resolutions['2x'], outputFileName);
+			} else if (icon.resolutions['3x'] !== undefined) {
+				const outputFileName = path.resolve(
+					`${path.dirname(file)}/icon-set-imagable`,
+					platform.iconPlatform,
+					icon.name,
+					`${icon.resolutions['3x']?.height}x${icon.resolutions['3x']?.width}.png`
+				);
+
+				await resize(file, icon.resolutions['3x'], outputFileName);
+			}
+		});
+
+		platform.spotlightIconSizes?.forEach(async (icon) => {
+			if (icon.resolutions['1x'] !== undefined) {
+				const outputFileName = path.resolve(
+					`${path.dirname(file)}/icon-set-imagable`,
+					platform.iconPlatform,
+					icon.name,
+					`${icon.resolutions['1x']?.height}x${icon.resolutions['1x']?.width}.png`
+				);
+
+				await resize(file, icon.resolutions['1x'], outputFileName);
+			} else if (icon.resolutions['2x'] !== undefined) {
+				const outputFileName = path.resolve(
+					`${path.dirname(file)}/icon-set-imagable`,
+					platform.iconPlatform,
+					icon.name,
+					`${icon.resolutions['2x']?.height}x${icon.resolutions['2x']?.width}.png`
+				);
+
+				await resize(file, icon.resolutions['2x'], outputFileName);
+			} else if (icon.resolutions['3x'] !== undefined) {
+				const outputFileName = path.resolve(
+					`${path.dirname(file)}/icon-set-imagable`,
+					platform.iconPlatform,
+					icon.name,
+					`${icon.resolutions['3x']?.height}x${icon.resolutions['3x']?.width}.png`
+				);
+
+				await resize(file, icon.resolutions['3x'], outputFileName);
+			}
 		});
 	}
 };
