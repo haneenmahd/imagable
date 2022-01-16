372bdca9a7221d41e1a6ba7c8adc5896f8062d44
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-16 13:17:38 +0530

    clean up user data dir (#37) (#33)

---
+++ server/src/utils/cleanUpIconData.ts
@@ -0,0 +1,13 @@
+import fs from 'fs';
+import path from 'path';
+
+export default function cleanUpIconData(): void {
+    const deletePath = path.resolve(process.cwd(), 'user-data', 'icon-set-imagable');
+
+    if (fs.existsSync(deletePath)) {
+        fs.rmSync(deletePath, { recursive: true });
+    }
+
+    // finalize with an empty directory
+    fs.mkdirSync(deletePath);
+}
