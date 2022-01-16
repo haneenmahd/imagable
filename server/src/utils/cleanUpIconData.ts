40035e2c450ae72b4d8eb4ef6459e1b2a563d41d
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-16 13:35:53 +0530

    fix up download errors for web [#37]

--- server/src/utils/cleanUpIconData.ts
+++ server/src/utils/cleanUpIconData.ts
@@ -2,12 +2,16 @@ import fs from 'fs';
 import path from 'path';
 
 export default function cleanUpIconData(): void {
-    const deletePath = path.resolve(process.cwd(), 'user-data', 'icon-set-imagable');
+	const deletePath = path.resolve(
+		process.cwd(),
+		'user-data',
+		'icon-set-imagable'
+	);
 
-    if (fs.existsSync(deletePath)) {
-        fs.rmSync(deletePath, { recursive: true });
-    }
+	if (fs.existsSync(deletePath)) {
+		fs.rmSync(deletePath, { recursive: true });
+	}
 
-    // finalize with an empty directory
-    fs.mkdirSync(deletePath);
+	// finalize with an empty directory
+	fs.mkdirSync(deletePath);
 }
