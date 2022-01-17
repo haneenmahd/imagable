7e67f1bf33e633c7f635381a0907c4fcebde2a29
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-17 22:44:46 +0530

    Format ✅

--- server/src/utils/generatePath.ts
+++ server/src/utils/generatePath.ts
@@ -1,5 +1,5 @@
 import getServerUrl from './getServerUrl';
 
 export default function generatePath(...files: string[]): string {
-    return `${getServerUrl()}/${files}`;
+	return `${getServerUrl()}/${files}`;
 }
