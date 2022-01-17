04e042d7aad7858ba77b6e20d6144a40b67d667a
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-17 19:41:02 +0530

    Prettier 🎊

--- web/src/globals.ts
+++ web/src/globals.ts
@@ -35,7 +35,7 @@ export function applyFilter(_input: FilterInput): FlattenSimpleInterpolation {
 export interface FileObject {
   folderName: string;
   files: string[];
-};
+}
 
 export interface ResponseDataObject {
   folderName: string;
