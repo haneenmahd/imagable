ee7b1f0add2bf0fea42afe5d2c234888dcdd812f
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-17 21:20:19 +0530

    Add commons docs

--- common/README.md
+++ common/README.md
@@ -1,1 +1,3 @@
 Folder to contain data that needs to be shared between `server` and `web` directories.
+
+- `models` directory contains typescript and javascript code which can be copy pasted into re-use.
--- common/models/getServerUrl.ts
+++ common/models/getServerUrl.ts
@@ -1,7 +1,7 @@
 export default function getServerUrl(): string {
-  if (process.env.NODE_ENV === 'production') {
-    return 'https://imagable.herokuapp.com';
+  if (process.env.NODE_ENV === "production") {
+    return "https://imagable.herokuapp.com";
   } else {
-    return 'http://localhost:3000';
+    return "http://localhost:3000";
   }
 }
