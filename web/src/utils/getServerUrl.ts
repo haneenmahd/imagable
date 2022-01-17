cb4c6978f70d4e9027ce939d6528f51e188f5ad5
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-17 21:20:10 +0530

    Move `getServerUrl` to `common`

--- web/src/utils/getServerUrl.ts
+++
@@ -1,7 +0,0 @@
-export default function getServerUrl(): string {
-    if (process.env.NODE_ENV === "production") {
-        return 'https://imagable.herokuapp.com';
-    } else {
-        return 'http://localhost:3000';
-    }
-}
