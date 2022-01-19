27600ee40d6128c9f62817732057ea8de53585a8
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-19 10:35:55 +0530

    add use-force hook

--- web/src/hooks/useForceUpdate.ts
+++ web/src/hooks/useForceUpdate.ts
@@ -1,7 +1,7 @@
 import { useState } from "react";
 
 export default function useForceUpdate() {
-    const [value, setValue] = useState(0);
-    
-    return () => setValue(value => value + 1);
+  const [value, setValue] = useState(0);
+
+  return () => setValue((value) => value + 1);
 }
