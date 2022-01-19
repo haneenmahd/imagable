7e18c88ec31364eae8266f8f32fce003608c750a
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-19 21:13:34 +0530

    Format ✅

--- web/src/components/Loading.tsx
+++ web/src/components/Loading.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import styled, { keyframes } from "styled-components";
 
 const LoadingStyle = styled.div`
-    display: flex;
-    align-items: center;
-    justify-content: center;
-    position: fixed;
-    top: 0;
-    left: 0;
-    height: 100vh;
-    width: 100vw;
-    background: #c4c4c450;
-    backdrop-filter: blur(10px);
-    -webkit-backdrop-filter: blur(10px);
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: fixed;
+  top: 0;
+  left: 0;
+  height: 100vh;
+  width: 100vw;
+  background: #c4c4c450;
+  backdrop-filter: blur(10px);
+  -webkit-backdrop-filter: blur(10px);
 `;
 
 const LoadingAnimation = keyframes`
@@ -26,19 +26,20 @@ const LoadingAnimation = keyframes`
 `;
 
 const Loader = styled.span`
-    height: 50px; width: 50px;
-    border-radius: 100px;
-    border: 5px solid #fff;
-    border-left: 5px solid #454545;
-    animation: ${LoadingAnimation} 1s infinite ease-in-out;
+  height: 50px;
+  width: 50px;
+  border-radius: 100px;
+  border: 5px solid #fff;
+  border-left: 5px solid #454545;
+  animation: ${LoadingAnimation} 1s infinite ease-in-out;
 `;
 
 const Loading = () => {
-    return (
-        <LoadingStyle>
-            <Loader />
-        </LoadingStyle>
-    );
-}
+  return (
+    <LoadingStyle>
+      <Loader />
+    </LoadingStyle>
+  );
+};
 
 export default Loading;
