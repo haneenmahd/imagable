c67a5cd67266a740eb0b628f2b9de16566e35cab
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-19 11:19:19 +0530

    fix codefactor styling

--- web/src/hooks/useForceUpdate.ts
+++ web/src/hooks/useForceUpdate.ts
@@ -3,5 +3,5 @@ import { useState } from "react";
 export default function useForceUpdate() {
   const [value, setValue] = useState(0);
 
-  return () => setValue((value) => value + 1);
+  return () => setValue(() => value + 1);
 }
