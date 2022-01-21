87f569cea43ca324f873b615580e334093946713
Haneen Mahdin <haneenmahdin@gmail.com>
2022-01-21 15:23:59 +0530

    Create footer style (#58)

--- web/src/components/Footer.tsx
+++ web/src/components/Footer.tsx
@@ -2,18 +2,14 @@ import React, { PureComponent } from "react";
 import styled from "styled-components";
 
 const FooterStyle = styled.footer`
-    height: 100px;
-    width: 100vw;
-    max-width: 100vw;
-    background: #333;
+  height: 100px;
+  width: 100vw;
+  max-width: 100vw;
+  background: #333;
 `;
 
 export default class Footer extends PureComponent {
-    render() {
-        return (
-            <FooterStyle>
-
-            </FooterStyle>
-        );
-    }
+  render() {
+    return <FooterStyle></FooterStyle>;
+  }
 }
